298fd33029432f5bf1d97b24d16e6a750df0f0f5
KreiserLee <crazieryang@me.com>
2016-11-25 18:11:15 +0800

    Problem No.3

--- leetcode/longestSubstringWithoutRepeatingCharactersBest.cpp
+++ leetcode/longestSubstringWithoutRepeatingCharactersBest.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <hash_map>
+using namespace std;
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int start = 0, end = 0, max = 0;          
-        map<char, int> charPoint;
-      
+        int charPoint[256] = {-1};
+        
+        // 为了不和0位置冲突
+        for (int i = 0; i < 256; ++i)
+            charPoint[i] = -1;
+        
         for (int i = 0; i < s.length(); ++i) {
-            if (charPoint.find(s.at(i)) != charPoint.end()) {
-                int point = charPoint[s.at(i)];
-                if (point >= start)
-                    start = point + 1;
-            }
+            if (charPoint[s[i]] >= start)
+                start = charPoint[s[i]] + 1;
             end = i;
-            charPoint[s.at(i)] = i;
+            charPoint[s[i]] = i;
             if (end-start+1 > max)
                 max = end-start+1;
         }
